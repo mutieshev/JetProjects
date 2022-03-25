f90989deb4766529c6b65374781d32a54edce2f0
mutieshev <mutieshev@griddynamics.com>
2022-03-25 10:51:27 +0200

    Final stage

--- src/battleship/Field.java
+++ src/battleship/Field.java
@@ -4,16 +4,22 @@ import java.util.Arrays;
 
 public class Field {
     private final char[][] field;
+    private int health;
 
     public Field() {
+        this.health = 0;
         this.field = new char[10][10];
         for (char[] chars : this.field) {
             Arrays.fill(chars, '~');
         }
     }
 
+    public int getHealth() {
+        return health;
+    }
+
     public void printField() {
-        System.out.print(" ");
+        System.out.print("\n ");
         for (int i = 0; i < this.field.length; i++) {
             System.out.printf(" %d", i + 1);
         }
@@ -29,9 +35,31 @@ public class Field {
         }
     }
 
+    public void printHiddenField() {
+        System.out.print(" ");
+        for (int i = 0; i < this.field.length; i++) {
+            System.out.printf(" %d", i + 1);
+        }
+        System.out.println();
+        char row;
+        for (int i = 0; i < field.length; i++) {
+            row = (char) ('A' + i);
+            System.out.printf("%c", row);
+            for (int j = 0; j < field[i].length; j++) {
+                if (this.field[i][j] == 'O') {
+                    System.out.print(" ~");
+                }
+                else {
+                    System.out.printf(" %c", this.field[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public boolean placeShip(int startRow, int startColumn, int endRow, int endColumn, Ship ship) {
         if (startRow != endRow && startColumn != endColumn) {
-            System.out.print("Error! Wrong ship location! Try again: ");
+            System.out.println("\nError! Wrong ship location! Try again:\n");
             return false;
         }
         else if (startRow == endRow) {
@@ -51,7 +79,7 @@ public class Field {
             endColumn = temp;
         }
         if ((endColumn - startColumn) + 1 != (ship.getSize())) {
-            System.out.printf("Error! Wrong length of the %s! Try again: ", ship.getName());
+            System.out.printf("\nError! Wrong length of the %s! Try again:\n\n", ship.getName());
             return false;
         }
 
@@ -64,7 +92,7 @@ public class Field {
                     continue;
                 }
                 if (this.field[r][c] == 'O') {
-                    System.out.print("Error! You placed it too close to another one. Try again: ");
+                    System.out.println("\nError! You placed it too close to another one. Try again:\n");
                     return false;
                 }
             }
@@ -73,6 +101,7 @@ public class Field {
         for (int col = startColumn; col <= endColumn; col++) {
             this.field[row][col] = 'O';
         }
+        this.health += ship.getSize();
         return true;
     }
 
@@ -84,7 +113,7 @@ public class Field {
             endRow = temp;
         }
         if ((endRow - startRow) + 1 != (ship.getSize())) {
-            System.out.printf("Error! Wrong length of the %s! Try again: ", ship.getName());
+            System.out.printf("\nError! Wrong length of the %s! Try again:\n\n", ship.getName());
             return false;
         }
 
@@ -97,7 +126,7 @@ public class Field {
                     continue;
                 }
                 if (this.field[r][c] == 'O') {
-                    System.out.print("Error! You placed it too close to another one. Try again: ");
+                    System.out.println("\nError! You placed it too close to another one. Try again:\n");
                     return false;
                 }
             }
@@ -106,6 +135,43 @@ public class Field {
         for (int row = startRow; row <= endRow; row++) {
             this.field[row][col] = 'O';
         }
+        this.health += ship.getSize();
         return true;
+    }
+
+    public boolean shot(int x, int y) {
+        if ((x < 0 || x > 9) || (y < 0 || y > 9)) {
+            System.out.println("\nError! You entered the wrong coordinates! Try again:\n");
+            return false;
+        }
+
+        if (field[y][x] == 'O') {
+            if (health != 0) {
+                field[y][x] = 'X';
+                health--;
+                printHiddenField();
+                System.out.println("\nYou hit a ship!");
+                return true;
+            }
+            else {
+                field[y][x] = 'X';
+                health--;
+                printHiddenField();
+                System.out.println("You sank the last ship. You won. Congratulations!");
+                return true;
+            }
+        }
+        else if (field[y][x] == '~') {
+            field[y][x] = 'M';
+            printHiddenField();
+            System.out.println("\nYou missed!");
+            return true;
+        }
+        else if (field[y][x] == 'X') {
+            printHiddenField();
+            System.out.print("\nYou hit a ship!");
+            return true;
+        }
+        return false;
     }
 }
--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -4,24 +4,83 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Field field = new Field();
+        Field[] game = new Field[2];
+        int player = 0;
+        boolean isWin = false;
         Ship[] ships = Ship.values();
         Scanner scanner = new Scanner(System.in);
-        field.printField();
-        for (Ship ship : ships) {
-            System.out.printf("Enter the coordinates of the %s (%d cells): ", ship.getName(), ship.getSize());
-            while (true) {
-                String coordinate1 = scanner.next();
-                String coordinate2 = scanner.next();
-                int row1 = coordinate1.charAt(0) - 65;
-                int col1 = Integer.parseInt(coordinate1.substring(1)) - 1;
-                int row2 = coordinate2.charAt(0) - 65;
-                int col2 = Integer.parseInt(coordinate2.substring(1)) - 1;
-                boolean isPLaced = field.placeShip(row1, col1, row2, col2, ship);
-                if (isPLaced) {
-                    field.printField();
-                    break;
+        for (int i = 0; i < 2; i++) {
+            game[i] = new Field();
+            System.out.println("Player " + (i + 1) + ", place your ships on the game field");
+            game[i].printField();
+            for (Ship ship : ships) {
+                System.out.printf("\nEnter the coordinates of the %s (%d cells):\n\n", ship.getName(), ship.getSize());
+                while (true) {
+                    String coordinate1 = scanner.next();
+                    String coordinate2 = scanner.next();
+                    int row1 = coordinate1.charAt(0) - 65;
+                    int col1 = Integer.parseInt(coordinate1.substring(1)) - 1;
+                    int row2 = coordinate2.charAt(0) - 65;
+                    int col2 = Integer.parseInt(coordinate2.substring(1)) - 1;
+                    boolean isPLaced = game[i].placeShip(row1, col1, row2, col2, ship);
+                    if (isPLaced) {
+                        game[i].printField();
+                        break;
+                    }
                 }
+            }
+            System.out.println("\nPress Enter and pass the move to another player");
+            scanner.nextLine();
+            scanner.nextLine();
+        }
+
+        while (!isWin) {
+            switch (player) {
+                case 0:
+                    game[1].printHiddenField();
+                    System.out.println("---------------------");
+                    game[0].printField();
+                    System.out.println("\nPlayer 1, it's your turn:");
+                    while (game[1].getHealth() != 0) {
+                        String shot = scanner.next();
+                        int x = Integer.parseInt(shot.substring(1)) - 1;
+                        int y = shot.charAt(0) - 65;
+                        boolean isShot = game[1].shot(x, y);
+                        if (isShot) {
+                            break;
+                        }
+                    }
+                    if (game[1].getHealth() == 0) {
+                        isWin = true;
+                        break;
+                    }
+                    player = 1;
+                    System.out.println("Press Enter and pass the move to another player");
+                    scanner.nextLine();
+                    scanner.nextLine();
+                    break;
+                case 1:
+                    game[0].printHiddenField();
+                    System.out.println("---------------------");
+                    game[1].printField();
+                    System.out.println("\nPlayer 2, it's your turn:");
+                    while (game[0].getHealth() != 0) {
+                        String shot = scanner.next();
+                        int x = Integer.parseInt(shot.substring(1)) - 1;
+                        int y = shot.charAt(0) - 65;
+                        boolean isShot = game[0].shot(x, y);
+                        if (isShot) {
+                            break;
+                        }
+                    }
+                    if (game[0].getHealth() == 0) {
+                        isWin = true;
+                        break;
+                    }
+                    player = 0;
+                    System.out.println("Press Enter and pass the move to another player");
+                    scanner.nextLine();
+                    scanner.nextLine();
             }
         }
     }
