388dec5c81e878499753b68afef11250a2e7a487
mutieshev <mutieshev@griddynamics.com>
2022-04-08 16:23:13 +0300

    Music advisor final stage

--- src/musicadvisor/Authorization.java
+++ src/musicadvisor/Authorization.java
@@ -1,6 +1,5 @@
-package musicadvisor;
+package advisor;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.sun.net.httpserver.HttpExchange;
@@ -13,32 +12,35 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class Authorization {
-    private String accessCode;
-    private String accessToken;
-    private final String spotifyApi = "https://api.spotify.com";
-    private final HttpClient client = HttpClient.newBuilder().build();
+    public static String SERVER_PATH = "https://accounts.spotify.com";
+    public static String REDIRECT_URI = "http://localhost:8080";
+    public static String CLIENT_ID = "bc28fe5be37d4ea49470556b006d4e55";
+    public static String CLIENT_SECRET = "430e02cd09ef4323be0d49a0eddc4721";
+    public static String ACCESS_TOKEN = "BQBfp1KxkrPxIlkwidhU2rhhK1J9YKNqNn9nPTl1G2quWdZI8CGgL0AHHZ38qSwsvBk2cGppSnHHWRSS82YqKtNbPZF3ixc9a_NOjU1GePKC37zlNWyAZ9vLH0btI96R4HZPM0oX_QufMu8MKMFkn8VHJL8cOKpSGg\n";
+    public static String API_SERVER_PATH = "https://api.spotify.com";
+    public static String ACCESS_CODE = "";
 
     public void getAccessCode() throws IOException, InterruptedException {
-        System.out.println("https://accounts.spotify.com/authorize?" +
-                "client_id=e200443d9f17416589e98951db551839" +
-                "&redirect_uri=http://localhost:8080/&response_type=code\n" +
-                "waiting for code...");
-
+        String uri = SERVER_PATH + "/authorize"
+                + "?client_id=" + CLIENT_ID
+                + "&redirect_uri=" + REDIRECT_URI
+                + "&response_type=code";
+        System.out.println("use this link to request the access code:");
+        System.out.println(uri);
         HttpServer server = HttpServer.create();
         server.bind(new InetSocketAddress(8080), 0);
+        server.start();
         server.createContext("/",
                 new HttpHandler() {
                     public void handle(HttpExchange exchange) throws IOException {
                         String query = exchange.getRequestURI().getQuery();
                         String request;
-                        if(query != null && query.contains("code")) {
-                            accessCode = query.substring(5);
+                        if (query != null && query.contains("code")) {
+                            ACCESS_CODE = query.substring(5);
                             System.out.println("code received");
+                            System.out.println(ACCESS_CODE);
                             request = "Got the code. Return back to your program.";
                         } else {
                             request = "Authorization code not found. Try again.";
@@ -47,174 +49,35 @@ public class Authorization {
                         exchange.getResponseBody().write(request.getBytes());
                         exchange.getResponseBody().close();
                     }
-                }
-        );
-        server.start();
-        while (accessCode == null) {
+                });
+        System.out.println("waiting for code...");
+        while (ACCESS_CODE.length() == 0) {
             Thread.sleep(100);
         }
-        server.stop(1);
+        server.stop(5);
     }
 
-    public void getToken() throws IOException, InterruptedException {
+    public void getAccessToken() throws IOException, InterruptedException {
         System.out.println("making http request for access_token...");
+        System.out.println("response:");
         HttpRequest request = HttpRequest.newBuilder()
                 .header("Content-Type", "application/x-www-form-urlencoded")
-                .uri(URI.create("https://accounts.spotify.com/api/token"))
+                .uri(URI.create(SERVER_PATH + "/api/token"))
                 .POST(HttpRequest.BodyPublishers.ofString(
                         "grant_type=authorization_code"
-                                + "&code=" + accessCode
-                                + "&client_id=e200443d9f17416589e98951db551839"
-                                + "&client_secret=e8f2d6158aad4dadb61b7d3676e909dd"
-                                + "&redirect_uri=http://localhost:8080/"))
+                                + "&code=" + ACCESS_CODE
+                                + "&client_id=" + CLIENT_ID
+                                + "&client_secret=" + CLIENT_SECRET
+                                + "&redirect_uri=" + REDIRECT_URI))
                 .build();
+
+        HttpClient client = HttpClient.newBuilder().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonObject token =
-                JsonParser.parseString(response.body()).getAsJsonObject();
-        accessToken = token.get("access_token").getAsString();
+        assert response != null;
+        System.out.println(response.body());
+        Main.isAuthorized = true;
+        JsonObject jo = JsonParser.parseString(response.body()).getAsJsonObject();
+        ACCESS_TOKEN = jo.get("access_token").getAsString();
         System.out.println("Success!");
-    }
-
-    public void getFeatured() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .header("Authorization", "Bearer " + accessToken)
-                .uri(URI.create(spotifyApi + "/v1/browse/featured-playlists"))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonObject featured = JsonParser.parseString(response.body()).getAsJsonObject();
-        JsonObject playlists = featured.getAsJsonObject("playlists");
-        JsonArray item = playlists.getAsJsonArray("items");
-        List<JsonObject> albums = new ArrayList<>();
-        for (int i = 0; i < item.size(); i++) {
-            albums.add(item.get(i).getAsJsonObject());
-        }
-        List<String> names = new ArrayList<>();
-        List<JsonObject> extUrls = new ArrayList<>();
-        for (JsonObject album : albums) {
-            names.add(album.get("name").getAsString());
-            extUrls.add(album.getAsJsonObject("external_urls"));
-        }
-        List<String> playlistUrls = new ArrayList<>();
-        for (JsonObject extUrl : extUrls) {
-            playlistUrls.add(extUrl.get("spotify").getAsString());
-        }
-        for (int i = 0; i < names.size(); i++) {
-            System.out.println(names.get(i));
-            System.out.println(playlistUrls.get(i) + "\n");
-        }
-    }
-
-    public void getNewReleases() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .header("Authorization", "Bearer " + accessToken)
-                .uri(URI.create(spotifyApi + "/v1/browse/new-releases"))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonObject newReleases = JsonParser.parseString(response.body()).getAsJsonObject();
-        JsonObject albums = newReleases.getAsJsonObject("albums");
-        JsonArray item = albums.getAsJsonArray("items");
-        List<JsonObject> songs = new ArrayList<>();
-        for (int i = 0; i < item.size(); i++) {
-            songs.add(item.get(i).getAsJsonObject());
-        }
-        List<String> names = new ArrayList<>();
-        List<List<String>> artistsNames = new ArrayList<>();
-        List<JsonArray> artists = new ArrayList<>();
-        List<JsonObject> extUrls = new ArrayList<>();
-        for (JsonObject song : songs) {
-            names.add(song.get("name").getAsString());
-            artists.add(song.getAsJsonArray("artists"));
-            artistsNames.add(new ArrayList<>());
-            extUrls.add(song.getAsJsonObject("external_urls"));
-        }
-        for (int i = 0; i < artists.size(); i++) {
-            for (int j = 0; j < artists.get(i).size(); j++) {
-                artistsNames.get(i).add(artists.get(i).get(j)
-                        .getAsJsonObject().get("name").getAsString());
-            }
-        }
-        List<String> songUrls = new ArrayList<>();
-        for (JsonObject extUrl : extUrls) {
-            songUrls.add(extUrl.get("spotify").getAsString());
-        }
-        for (int i = 0; i < names.size(); i++) {
-            System.out.println(names.get(i));
-            System.out.println(artistsNames.get(i).toString());
-            System.out.println(songUrls.get(i) + "\n");
-        }
-    }
-
-    public void getCategories() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .header("Authorization", "Bearer " + accessToken)
-                .uri(URI.create(spotifyApi + "/v1/browse/categories"))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonObject allCategories =
-                JsonParser.parseString(response.body()).getAsJsonObject().getAsJsonObject("categories");
-        JsonArray items = allCategories.getAsJsonArray("items");
-        List<JsonObject> categories = new ArrayList<>();
-        for (int i = 0; i < items.size(); i++) {
-            categories.add(items.get(i).getAsJsonObject());
-        }
-        List<String> categoriesNames = new ArrayList<>();
-        for (JsonObject category : categories) {
-            categoriesNames.add(category.get("name").getAsString());
-        }
-        for (String categoriesName : categoriesNames) {
-            System.out.println(categoriesName);
-        }
-    }
-
-    public String getCategoryID(String categoryName) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .header("Authorization", "Bearer " + accessToken)
-                .uri(URI.create(spotifyApi + "/v1/browse/categories"))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonObject allCategories =
-                JsonParser.parseString(response.body()).getAsJsonObject().getAsJsonObject("categories");
-        JsonArray items = allCategories.getAsJsonArray("items");
-        List<JsonObject> categories = new ArrayList<>();
-        for (int i = 0; i < items.size(); i++) {
-            categories.add(items.get(i).getAsJsonObject());
-        }
-        String categoryID;
-        for (JsonObject category : categories) {
-            if (Objects.equals(categoryName, category.get("name").getAsString())) {
-                categoryID = category.get("id").getAsString();
-                return categoryID;
-            }
-        }
-        return "Unknown category name.";
-    }
-
-    public void getPlaylistsByName(String categoryID) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .header("Authorization", "Bearer " + accessToken)
-                .uri(URI.create(spotifyApi + "/v1/browse/categories/" + categoryID + "/playlists"))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonObject allPlaylists =
-                JsonParser.parseString(response.body()).getAsJsonObject().getAsJsonObject("playlists");
-        JsonArray items = allPlaylists.getAsJsonArray("items");
-        List<JsonObject> playlists = new ArrayList<>();
-        for (int i = 0; i < items.size(); i++) {
-            playlists.add(items.get(i).getAsJsonObject());
-        }
-        List<String> playlistsNames = new ArrayList<>();
-        List<JsonObject> extUrls = new ArrayList<>();
-        for (JsonObject playlist : playlists) {
-            playlistsNames.add(playlist.get("name").getAsString());
-            extUrls.add(playlist.getAsJsonObject("external_urls"));
-        }
-        List<String> playlistUrls = new ArrayList<>();
-        for (JsonObject extUrl : extUrls) {
-            playlistUrls.add(extUrl.get("spotify").getAsString());
-        }
-        for (int i = 0; i < playlistsNames.size(); i++) {
-            System.out.println(playlistsNames.get(i));
-            System.out.println(playlistUrls.get(i) + "\n");
-        }
     }
 }
--- src/musicadvisor/Main.java
+++ src/musicadvisor/Main.java
@@ -1,60 +1,44 @@
-package musicadvisor;
+package advisor;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    static boolean isAuthorized = false;
+    public static int PAGES = 5;
+    static Viewers current;
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        musicadvisor.Authorization authorization = new musicadvisor.Authorization();
-        Scanner scan = new Scanner(System.in);
-        String menuRequest;
-        boolean on = true;
-        boolean auth = false;
-        while (!auth) {
-            menuRequest = scan.nextLine();
-            switch (menuRequest) {
-                case "auth":
-                    authorization.getAccessCode();
-                    authorization.getToken();
-                    auth = true;
-                    break;
-                case "exit":
-                    System.out.println("---GOODBYE!---");
-                    System.exit(-1);
-                default:
-                    System.out.println("Please, provide access for application.");
-            }
-        }
-        while (on) {
-            menuRequest = scan.next();
-            switch (menuRequest) {
-                case "featured":
-                    authorization.getFeatured();
-                    break;
-                case "new":
-                    authorization.getNewReleases();
-                    break;
-                case "categories":
-                    authorization.getCategories();
-                    break;
-                case "playlists":
-                    String categoryName = scan.nextLine();
-                    System.out.println(categoryName.trim());
-                    String categoryID = authorization.getCategoryID(categoryName.trim());
-                    if (!Objects.equals(categoryID, "Unknown category name.")) {
-                        authorization.getPlaylistsByName(categoryID);
-                    }
-                    else {
-                        System.out.println(categoryID);
-                    }
-                    break;
-                case "exit":
-                    System.out.println("---GOODBYE!---");
-                    on = false;
-                    break;
-                default:
-                    System.out.println("You enter wrong command");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine().trim();
+            if (command.equals("exit")) {
+                break;
+            } else if (command.equals("auth")) {
+                Authorization authorisation = new Authorization();
+                authorisation.getAccessCode();
+                authorisation.getAccessToken();
+            } else if (!isAuthorized) {
+                System.out.println("Please, provide access for application.");
+            } else if (command.equals("categories")) {
+                current = new Categories();
+                current.printPlaylist();
+            } else if (command.equals("featured")) {
+                current = new Featured();
+                current.printPlaylist();
+            } else if (command.equals("new")) {
+                current = new NewSongs();
+                current.printPlaylist();
+            } else if (command.contains("playlists ")) {
+                String type = command.trim().substring(10);
+                current = new ParticularPlaylist(type);
+                current.printPlaylist();
+            } else if (command.equals("prev")) {
+                current.prev();
+            } else if (command.equals("next")) {
+                current.next();
+            } else {
+                System.out.println("Enter Valid Command.");
             }
         }
     }
